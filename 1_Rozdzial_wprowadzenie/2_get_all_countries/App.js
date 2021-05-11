3afa5f73e513e0d41d3d8b17b1d8a47d4288b3d5
kazikkoduje <kazik@kazikkoduje.pl>
2021-05-11 17:06:58 +0200

    done

--- 1_Rozdzial_wprowadzenie/2_get_all_countries/App.js
+++ 1_Rozdzial_wprowadzenie/2_get_all_countries/App.js
@@ -4,10 +4,30 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   FlatList,
   ActivityIndicator,
   SafeAreaView,
 } from "react-native";
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
 
 const API_URL =
   "https://restcountries.eu/rest/v2/alpha?codes=af;al;dz;as;ad;ao;ai;aq;ag;ar;am;aw;au;at;az;bs;bh;bd;bb;by;be;bz;bj;bm;bt;bo;bq;ba;bw;bv;br;io;bn;bg;bf;bi;cv;kh;cm;ca;ky;cf;td;cl;cn;cx;cc;co;km;cd;cg;ck;cr;hr;cu;cw;cy;cz;ci;dk;dj;dm;do;ec;eg;sv;gq;er;ee;sz;et;fk;fo;fj;fi;fr;gf;pf;tf;ga;gm;ge;de;gh;gi;gr;gl;gd;gp;gu;gt;gg;gn;gw;gy;ht;hm;va;hn;hk;hu;is;in;id;ir;iq;ie;im;il;it;jm;jp;je;jo;kz;ke;ki;kp;kr;kw;kg;la;lv;lb;ls;lr;ly;li;lt;lu;mo;mg;mw;my;mv;ml;mt;mh;mq;mr;mu;yt;mx;fm;md;mc;mn;me;ms;ma;mz;mm;na;nr;np;nl;nc;nz;ni;ne;ng;nu;nf;mp;no;om;pk;pw;ps;pa;pg;py;pe;ph;pn;pl;pt;pr;qa;mk;ro;ru;rw;re;bl;sh;kn;lc;mf;pm;vc;ws;sm;st;sa;sn;rs;sc;sl;sg;sx;sk;si;sb;so;za;gs;ss;es;lk;sd;sr;sj;se;ch;sy;tw;tj;tz;th;tl;tg;tk;to;tt;tn;tr;tm;tc;tv;ug;ua;ae;gb;um;us;uy;uz;vu;ve;vn;vg;vi;wf;eh;ye;zm;zw;ax";
@@ -27,17 +47,43 @@ export default function App() {
     })();
   }, []);
 
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.name}</Text>
-    </View>
-  );
+  const renderItem = ({ item }) => {
+    console.log(item);
+    return (
+      <View style={styles.item}>
+        {/* <Image
+          style={{
+            width: 40,
+            height: 40,
+            maxWidth: 40,
+            flex: 1,
+          }}
+          source={{
+            uri: `https://www.countryflags.io/${item.alpha2Code.toLocaleLowerCase()}/flat/64.png`,
+          }}
+        ></Image>
+        <Text>Czas: {item.timezones[0].substring(3)}</Text> */}
+        <Text style={styles.title}>{item.name}</Text>
+        <Text>{item.nativeName}</Text>
+        <Text>
+          Dist: 
+          {
+          getDistanceFromLatLonInKm(
+            53.2,
+            22.04,
+            item.latlng[0],
+            item.latlng[1]
+          )}
+        </Text>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.topBar}></View>
       <StatusBar />
-      {!countries.length ? (
+      {countries.length === 0 ? (
         <View style={styles.container}>
           <ActivityIndicator />
         </View>
@@ -67,14 +113,14 @@ const styles = StyleSheet.create({
     height: 52,
     flexDirection: "row",
     backgroundColor: "#36213E",
-    color: 'white',
+    color: "white",
     alignSelf: "stretch",
     justifyContent: "space-between",
     paddingLeft: 10,
     paddingRight: 10,
   },
   title: {
-    color: '#B8F3FF'
+    color: "#B8F3FF",
   },
   topBar: {
     alignSelf: "stretch",
