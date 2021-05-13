5ad58e129ed28f892ac691faf90e3d407e61d814
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2021-05-13 09:29:35 +0200

    add Name and Surname component

--- 3_React_architektura_frameworka/Zadania/App.js
+++ 3_React_architektura_frameworka/Zadania/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Name from "./Name";
+import Surname from "./Surname";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Name />
+      <Surname />
     </View>
   );
 }
