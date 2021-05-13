a8d0346b929ea98fb6ff9a47dbe383681c759605
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2021-05-13 14:00:12 +0200

    zadania 8-10

--- 3_React_architektura_frameworka/Zadania/App.js
+++ 3_React_architektura_frameworka/Zadania/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, StyleSheet, View, Text} from 'react-native';
+import Timer from "./Zadanie8/Timer";
 import Name from "./Zadanie1/Name";
 import Surname from "./Zadanie1/Surname";
-import SquareBox from "./Zadanie2/SquareBox";
-import Clock from "./Zadanie3/Clock";
-import Map from "./Zadanie4/Map";
-import Grid from "./Zadanie5/Grid";
+import Calendar from "./Zadanie3/Calendar";
+import Pizza from "./Zadanie6/Pizza";
+import Invitation from "./Zadanie7/Invitation";
 
 export default function App() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [randonNumber, setRandomNumber] = useState(0);
+    const inviteData = {
+        place: "domowka",
+        date: "sobota",
+        time: "16",
+        dressCode: "brak"
+    }
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+   useEffect(() => {
+       setInterval(() => {
+           setTime(new Date().toLocaleTimeString())
+       }, 1000);
+   }, []);
     return (
         <View style={styles.container}>
-            <Name />
-            <Surname />
-            <Clock/>
+            {/*<Name />*/}
+            {/*<Surname />*/}
+            {/*<Calendar/>*/}
             {/*<Map url={"https://bliskopolski.pl/pliki/mapa-polski-1701.png"}/>*/}
             {/*<SquareBox/>*/}
-            <Grid/>
+            {/*<Grid/>*/}
+            {/*<Pizza/>*/}
+            {/*<Invitation inviteData={inviteData}/>*/}
+            <Timer time={time}/>
+            <Text>{randonNumber}</Text>
+            <Button
+                title="Losuj"
+                onPress={() => setRandomNumber(getRandomInt(1,10))}
+            />
         </View>
     );
 }
--- 3_React_architektura_frameworka/Zadania/Zadanie3/Clock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Text} from "react-native";
-
-const Clock = () => {
-    const time = new Date().toLocaleTimeString();
-    return <Text>{time}</Text>
-};
-
-export default Clock;
