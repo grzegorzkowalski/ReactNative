65219b3a9d15375783b1dd36455ed1bfbc3ae210
kazikkoduje <kazik@kazikkoduje.pl>
2021-05-25 15:37:15 +0200

    done

---
+++ 8_Autentykacja/1_uwierzytelnienie/App.js
@@ -0,0 +1,40 @@
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+} from "react-native";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//Firebase config
+var firebaseConfig = {
+  apiKey: "AIzaSyBl2mbAxVASoQ7jJdx9kcCRM-GM2avRmqE",
+  authDomain: "coderlslab.firebaseapp.com",
+  databaseURL: "https://coderlslab.firebaseio.com",
+  projectId: "coderlslab",
+  storageBucket: "coderlslab.appspot.com",
+  messagingSenderId: "490304552202",
+  appId: "1:490304552202:web:b6ad68334b53ce161fcef3",
+};
+// Zainicjuj Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export default function App() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ 8_Autentykacja/1_uwierzytelnienie/README.md
@@ -0,0 +1,8 @@
+# Firebase Autentykacja | Konto użytkownika
+
+1. Stwórz Stack Navigator z ekranami: Home / Logowanie / Rejestracja / Profil użytkownika
+2. Ekran Home powinien zawierać dwa buttony kierujące do ekranów: Logowanie i Rejestracja
+3. Stwórz Formularz Rejestracji w ekranie: Rejestracja. Formularz rejestracji powinien oprócz loginu i hasła zawierać też podstawowe informacje o użytkowniku: Imię, wiek, zdjęcie (jako ulr do zewnętrznych zasobów – jeśli nie chcesz umieszczać swojego zdjęcia wybierz jakieś randomowe). Pamiętaj, że dodatkowe dane muszą zostać zapisane w odpowiedniej kolekcji np. users.
+4. Stwórz Formularz Logowanie w ekranie: Logowanie. Po zalogowaniu przekieruj użytkownika do jego profilu.
+5. Ekran Profil powinien wyświetlać dane użytkownika.
+6. Zabezpiecz dostęp do kolekcji dla jej właściciela
